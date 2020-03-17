a08be6059ec71e0f0f4823620b2f1560ffefe3be
patrickctrf <patrickctrf@gmail.com>
2020-03-17 16:55:08 -0300

    Trab 0 está terminado. Apenas Salvando.
    Não foi possível submeter, pois o link não estava disponível e acabei só fazendo o código mesmo.
    Já foi o suficiente para praticar.

--- trab 0/main.py
+++ trab 0/main.py
@@ -2,8 +2,7 @@ import cv2
 import numpy as np
 from skimage import exposure
 
-
-#===============Questao-1.1=====================================================
+# ==============Questao-1.1=====================================================
 
 # Abrimos a imagem como escala de cinza. Queremos a matriz de representacao.
 matrizImagem = cv2.imread("city.png", cv2.IMREAD_GRAYSCALE)
@@ -17,7 +16,7 @@ matrizImagemComplementar = cv2.bitwise_not(matrizImagem)
 # item ii: Contraste alterado.
 # Esta instrução apenas faz uma interpolação dos valores na faixa de 0 a 255
 # para a faixa de 100 a 200, implementada vetorialmente pela biblioteca numpy.
-matrizImagemConstraste = np.interp(matrizImagem, [0,255], [100,200]).astype('uint8')
+matrizImagemConstraste = np.interp(matrizImagem, [0, 255], [100, 200]).astype('uint8')
 
 # item iii: Inverter linhas pares da matriz.
 # Para tal, a funcao "range" gera o numero de cada linha da matriz de entrada
@@ -30,7 +29,7 @@ matrizImagemLinhasParesInvertidas = np.where(np.array(range(0, matrizImagem.shap
 # item iv: Espelhar a matriz de imagem na metade (verticalmente).
 # Para espelhar verticalmente, apenas recortamos a metade superior da matriz e
 # concatenamos com ela mesma invertida (funcao "flip").
-matrizImagemEspelhada = np.concatenate((matrizImagem[0:int(matrizImagem.shape[0]/2)], np.flip(matrizImagem[0:int(matrizImagem.shape[0]/2)], axis=0)), axis=0)
+matrizImagemEspelhada = np.concatenate((matrizImagem[0:int(matrizImagem.shape[0] / 2)], np.flip(matrizImagem[0:int(matrizImagem.shape[0] / 2)], axis=0)), axis=0)
 
 cv2.imshow("Original", matrizImagem)
 cv2.waitKey(0)
@@ -43,10 +42,10 @@ cv2.waitKey(0)
 cv2.imshow("Espelhada ao Meio Verticalmente", matrizImagemEspelhada)
 cv2.waitKey(0)
 
-#===============FIM-da-Questao-1.1==============================================
+# ==============FIM-da-Questao-1.1==============================================
 
 
-#===============Questao-1.2=====================================================
+# ==============Questao-1.2=====================================================
 
 # Abrimos a imagem como escala de cinza. Queremos a matriz de representacao.
 matrizImagem = cv2.imread("baboon.png", cv2.IMREAD_GRAYSCALE)
@@ -54,9 +53,9 @@ matrizImagem = cv2.imread("baboon.png", cv2.IMREAD_GRAYSCALE)
 # A funcao "adjust_gamma" exerce a correção de gamma proposta no roteiro, com a
 # unica diferenca que o scikit-imagem considera gamma elevado a "-1", em relacao
 # ao pedido no roteiro.
-matrizImagemGamma1_5 = exposure.adjust_gamma(matrizImagem, gamma=1/1.5)
-matrizImagemGamma2_5 = exposure.adjust_gamma(matrizImagem, gamma=1/2.5)
-matrizImagemGamma3_5 = exposure.adjust_gamma(matrizImagem, gamma=1/3.5)
+matrizImagemGamma1_5 = exposure.adjust_gamma(matrizImagem, gamma=1 / 1.5)
+matrizImagemGamma2_5 = exposure.adjust_gamma(matrizImagem, gamma=1 / 2.5)
+matrizImagemGamma3_5 = exposure.adjust_gamma(matrizImagem, gamma=1 / 3.5)
 
 cv2.imshow("Gamma 1.0", matrizImagem)
 cv2.waitKey(0)
@@ -67,10 +66,10 @@ cv2.waitKey(0)
 cv2.imshow("Gamma 3.5", matrizImagemGamma3_5)
 cv2.waitKey(0)
 
-#===============FIM-da-Questao-1.2==============================================
+# ==============FIM-da-Questao-1.2==============================================
 
 
-#===============Questao-1.3=====================================================
+# ==============Questao-1.3=====================================================
 
 # Abrimos a imagem como escala de cinza. Queremos a matriz de representacao.
 matrizImagem = cv2.imread("baboon.png", cv2.IMREAD_GRAYSCALE)
@@ -82,28 +81,27 @@ matrizImagem = cv2.imread("baboon.png", cv2.IMREAD_GRAYSCALE)
 # e um, formando uma representacao binaria.
 # O OpenCV2 mapeia automaticamente valores no intervalo [0,1] para o intervalo
 # [0,255]. https://docs.opencv.org/2.4/modules/highgui/doc/user_interface.html?highlight=imshow#cv2.imshow
-#
 cv2.imshow("Todos os planos", matrizImagem)
 cv2.waitKey(0)
-cv2.imshow("Plano de bit 0", (matrizImagem & 0b00000001)/0b00000001)
+cv2.imshow("Plano de bit 0", (matrizImagem & 0b00000001) / 0b00000001)
 cv2.waitKey(0)
-cv2.imshow("Plano de bit 4", (matrizImagem & 0b00010000)/0b00010000)
+cv2.imshow("Plano de bit 4", (matrizImagem & 0b00010000) / 0b00010000)
 cv2.waitKey(0)
-cv2.imshow("Plano de bit 7", (matrizImagem & 0b10000000)/0b10000000)
+cv2.imshow("Plano de bit 7", (matrizImagem & 0b10000000) / 0b10000000)
 cv2.waitKey(0)
 
-#===============FIM-da-Questao-1.3==============================================
+# ==============FIM-da-Questao-1.3==============================================
 
-#===============Questao-1.4=====================================================
+# ==============Questao-1.4=====================================================
 
 # Abrimos a imagem como escala de cinza. Queremos a matriz de representacao.
 matrizImagem = cv2.imread("baboon.png", cv2.IMREAD_GRAYSCALE)
 
 matrizImagemMosaico = np.concatenate((
-    np.concatenate((matrizImagem[128:256,128:256], matrizImagem[256:384,256:384], matrizImagem[384:512,0:128], matrizImagem[0:128,256:384]), axis=1),
-    np.concatenate((matrizImagem[128:256,384:512], matrizImagem[384:512,384:512], matrizImagem[0:128,0:128], matrizImagem[256:384,0:128]), axis=1),
-    np.concatenate((matrizImagem[256:384,384:512], matrizImagem[384:512,128:256], matrizImagem[0:128,128:256], matrizImagem[128:256,256:384]), axis=1),
-    np.concatenate((matrizImagem[0:128,384:512], matrizImagem[384:512,256:384], matrizImagem[256:384,128:256], matrizImagem[128:256,0:128]), axis=1)),
+    np.concatenate((matrizImagem[128:256, 128:256], matrizImagem[256:384, 256:384], matrizImagem[384:512, 0:128], matrizImagem[0:128, 256:384]), axis=1),
+    np.concatenate((matrizImagem[128:256, 384:512], matrizImagem[384:512, 384:512], matrizImagem[0:128, 0:128], matrizImagem[256:384, 0:128]), axis=1),
+    np.concatenate((matrizImagem[256:384, 384:512], matrizImagem[384:512, 128:256], matrizImagem[0:128, 128:256], matrizImagem[128:256, 256:384]), axis=1),
+    np.concatenate((matrizImagem[0:128, 384:512], matrizImagem[384:512, 256:384], matrizImagem[256:384, 128:256], matrizImagem[128:256, 0:128]), axis=1)),
     axis=0)
 
 cv2.imshow("Nao Mosaico", matrizImagem)
@@ -111,33 +109,23 @@ cv2.waitKey(0)
 cv2.imshow("Mosaico", matrizImagemMosaico)
 cv2.waitKey(0)
 
-#===============FIM-da-Questao-1.4==============================================
+# ==============FIM-da-Questao-1.4==============================================
 
-#===============Questao-1.5=====================================================
+# ==============Questao-1.5=====================================================
 
 # Abrimos as imagens como escala de cinza. Queremos as matrizes de representacao.
 babuinoImagem = cv2.imread("baboon.png", cv2.IMREAD_GRAYSCALE)
 borboletaImagem = cv2.imread("butterfly.png", cv2.IMREAD_GRAYSCALE)
 
-
-
 cv2.imshow("Babuino", babuinoImagem)
 cv2.waitKey(0)
 cv2.imshow("Borboleta", borboletaImagem)
 cv2.waitKey(0)
-cv2.imshow("Babuino02 + Borboleta08", (0.2*babuinoImagem + 0.8*borboletaImagem).astype('uint8'))
+cv2.imshow("Babuino02 + Borboleta08", (0.2 * babuinoImagem + 0.8 * borboletaImagem).astype('uint8'))
 cv2.waitKey(0)
-cv2.imshow("Babuino05 + Borboleta05", (0.5*babuinoImagem + 0.5*borboletaImagem).astype('uint8'))
+cv2.imshow("Babuino05 + Borboleta05", (0.5 * babuinoImagem + 0.5 * borboletaImagem).astype('uint8'))
 cv2.waitKey(0)
-cv2.imshow("Babuino08 + Borboleta02", (0.8*babuinoImagem + 0.2*borboletaImagem).astype('uint8'))
+cv2.imshow("Babuino08 + Borboleta02", (0.8 * babuinoImagem + 0.2 * borboletaImagem).astype('uint8'))
 cv2.waitKey(0)
 
-#===============FIM-da-Questao-1.5==============================================
-
-
-
-
-
-
-
-print(matrizImagem)
+# ==============FIM-da-Questao-1.5==============================================
