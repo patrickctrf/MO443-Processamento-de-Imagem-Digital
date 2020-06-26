5ecd03cb81956b7890b4310b949cc9ecce6ac278
Patrick de Carvalho Tavares Rezende Ferreira <ra175480@students.ic.unicamp.br>
2020-06-26 05:39:26 -0300

    Passo 8 terminado

--- trab-04/main.py
+++ trab-04/main.py
@@ -4,19 +4,53 @@ import cv2
 import numpy as np
 
 
-def imshow_components(labels):
-    # Map component labels to hue val
-    label_hue = np.uint8(179 * labels / np.max(labels))
-    blank_ch = 255 * np.ones_like(label_hue)
-    labeled_img = cv2.merge([label_hue, blank_ch, blank_ch])
+def draw_bounding_boxes(image, contornos):
+    for i, c in enumerate(contornos):
+        rect = cv2.boundingRect(c)
+        x, y, w, h = rect
+        image = cv2.rectangle(image, (x, y), (x + w, y + h), (255, 255, 255), 1)
+        image = cv2.putText(image, str(i), (x - 20, y + h), 0, 0.3, (255, 255, 255))
 
-    # cvt to BGR for display
-    labeled_img = cv2.cvtColor(labeled_img, cv2.COLOR_HSV2BGR)
+    return image
 
-    # set bg label to black
-    labeled_img[label_hue == 0] = 0
 
-    return labeled_img
+def get_ratios(image, contornos):
+    # Salvamos as porcentagens de cada parametro.
+    list_black_percentiles = []
+    list_transitions = []
+    for i, c in enumerate(contornos):
+        rect = cv2.boundingRect(c)
+        # w = width, h = height.
+        x, y, w, h = rect
+        # Montamos o contorno da imagem de entrada em si
+        img_snip = image[y:y + h, x:x + w]
+        # Mais facil para a logica booleana da transicao falso-verdadeiro.
+        # 255 vira 1, zero continua zero.
+        img_snip = np.where(img_snip == 0, 0, 1)
+
+        # Percentual entre pixels pretos e totais
+        black_pixels = np.count_nonzero(img_snip != 0)
+        total_pixels = w * h
+        list_black_percentiles.append(black_pixels / total_pixels)
+
+        # Numero de transicoes horizontais entre pixels pretos e brancos
+        indice_de_cada_transicao = np.where(np.roll(img_snip, shift=1, axis=1) < img_snip)[1]
+        # Transicao no indice zero indica apenas que o ultimo pixels eh diferente do primeiro, nao queremos contar isso.
+        indice_de_cada_transicao = np.delete(indice_de_cada_transicao, np.where(indice_de_cada_transicao == 0))
+        n_transicoes_horizontais = len(indice_de_cada_transicao)
+
+        # Numero de transicoes verticais entre pixels pretos e brancos
+        indice_de_cada_transicao = np.where(np.roll(img_snip, shift=1, axis=0) < img_snip)[1]
+        # Transicao no indice zero indica apenas que o ultimo pixels eh diferente do primeiro, nao queremos contar isso.
+        indice_de_cada_transicao = np.delete(indice_de_cada_transicao, np.where(indice_de_cada_transicao == 0))
+        n_transicoes_verticais = len(indice_de_cada_transicao)
+
+        if black_pixels != 0:
+            list_transitions.append((n_transicoes_verticais + n_transicoes_horizontais) / black_pixels)
+        else:
+            list_transitions.append(0)
+
+    return list_black_percentiles, list_transitions
 
 
 if __name__ == '__main__':
@@ -76,10 +110,17 @@ Executing examples and generating report outputs.
     cv2.imwrite("output/item6.pbm", imagem_closing_6)
 
     # ==============ITEM-7======================================================
-    # Garante que a imagem eh binaria
-    imagem_closing_7 = cv2.threshold(imagem_closing_6, 127, 255, cv2.THRESH_BINARY)[1]
-    n_connected_components_7, imagem_connected_components_7 = cv2.connectedComponents(imagem_closing_7)
-    imagem_colorida_componentes_7 = imshow_components(imagem_connected_components_7)
-    cv2.imshow("Item 7", imagem_colorida_componentes_7)
+    contornos, hierarquia = cv2.findContours(imagem_closing_6, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    imagem_componentes_conexos_contornados_7 = imagem_closing_6
+    imagem_componentes_conexos_contornados_7 = draw_bounding_boxes(imagem_componentes_conexos_contornados_7, contornos)
+
+    n_contornos_item_7 = len(contornos)
+    print("Encontrados ", n_contornos_item_7, " contornos.")
+    cv2.imshow("Item 7", imagem_componentes_conexos_contornados_7)
     cv2.waitKey(0)
-    cv2.imwrite("output/item7.png", imagem_colorida_componentes_7)
+    cv2.imwrite("output/item7.pbm", imagem_componentes_conexos_contornados_7)
+
+    # ==============ITEM-8======================================================
+    contornos, hierarquia = cv2.findContours(imagem_closing_6, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    imagem_componentes_conexos_contornados_7 = imagem_closing_6
+    list_black_percentiles_item_8, list_transitions_item_8 = get_ratios(imagem_componentes_conexos_contornados_7, contornos)
